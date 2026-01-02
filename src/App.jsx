db9abb5a834b6a6084997d323ff95bec29a5c606
Keona <kohorstkeona@outlook.com>
2026-01-02 12:55:24 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { NavBar } from './components/NavBar';
+import { Banner } from './components/Banner';
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <NavBar/>
+      <Banner />
+    </>
+  )
+}
+
+export default App
